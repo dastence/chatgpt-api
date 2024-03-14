44ef3d5af3998b989119e6cad793685b6b2c8e10
LZJ <2239882596@qq.com>
2024-03-14 23:50:35 +0800

    这是我第一次使用git

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiAplication.java
@@ -0,0 +1,19 @@
+package cn.bugstack.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ApiAplication
+ * @Description TODO
+ * @date 2024/3/14 22:14
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class ApiAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAplication.class,args);
+    }
+
+}
